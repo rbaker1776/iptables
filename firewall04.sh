932ef3d4af9eab82c6dd12a340cee7df8048c482
Ryan Baker <rbaker@xtier.com>
2025-04-03 16:50:14 -0400

    added tex file

--- firewall04.sh
+++ firewall04.sh
@@ -11,17 +11,18 @@ sudo iptables -A INPUT -s f1.com -j REJECT
 # enable MASQUERADE for outgoing packets
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# protect port scanning
+# protect against indiscriminate and nonstop port scanning
 sudo iptables -N PORTSCAN
 sudo iptables -A PORTSCAN -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j RETURN
 sudo iptables -A PORTSCAN -j DROP
 sudo iptables -A INPUT -p tcp --tcp-flags ALL SYN,ACK -j PORTSCAN
 
 # block SYN flooding
+# limit input to one per second after the 500th packet
 sudo iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 500 -j ACCEPT
 sudo iptables -A INPUT -p tcp --syn -j DROP
 
-# loopback
+# allow full loopback access
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 
